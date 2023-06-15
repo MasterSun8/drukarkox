96cb076e6d6716ccd9157ae9865c26b0c42023bb
MasterSun8 <kacpi.doro@gmail.com>
2023-06-15 23:26:24 +0200

    backend infinito

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const fs = require('fs')
 
 const src = fs.readdirSync('src')
 const img = fs.readdirSync('img')
+const db = fs.readdirSync('db')
 
 const public = ['drukarkox', 'leaderboard', 'graph', 'myprinters', 'settings']
 
@@ -18,18 +19,27 @@ const port = 3000
 
 const server = http.createServer((req, res) => {
     res.statusCode = 200
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET')
+    res.setHeader('Access-Control-Max-Age', 2592000)
     res.setHeader('Content-Type', 'text/html')
   
-    let url = req.url
-    console.log(url)
+    let url = req.url.substring(1)
+
+    let d = url.split('/')
+    let site = url.split('.')
     
-    
-    if(src.includes(url.substring(1))){
-        res.write(getSite('src'+url))
-    }else if(img.includes(url.substring(1))){
-        res.write(getSite('img'+url))
-    }else if(public.includes(url.substring(1).split('.')[0])){
-        url = url.substring(1).split('.')[0]
+    if(src.includes(url)){
+        res.write(getSite('src/'+url))
+    }else if(img.includes(url)){
+        res.write(getSite('img/'+url))
+    }else if(db.includes(d[1]) && d[0] == 'db'){
+        res.write(getSite('db/'+d[1]))
+    }else if(site[0]=='settings'){
+        res.write(getSite('public/'+'settings.html'))
+    }else if(public.includes(site[0])){
+        url = url.split('.')[0]
+        console.log(url)
         res.write('<html><head><link rel="stylesheet" href="main.css"><link rel="stylesheet" href="'+url+'.css"><script src="'+url+'.js"></script></head>')
         res.write(getSite('public/template.html'))
         res.write('<script src="main.js"></script></body></html>')
--- src/main.js
+++ src/main.js
@@ -1,7 +1,4 @@
 const url = location.pathname.substring(1).split('.')[0]
 const elem = document.getElementById(url)
 
-console.log(url)
-console.log(elem)
-
 elem.classList.add('active')
